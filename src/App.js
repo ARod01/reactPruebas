a315be4fdfb4ed3d20a9f68af8a5296998231d69
alfrerodriguez01@gmail.com <alfrerodriguez01@gmail.com>
2020-12-30 12:17:45 -0600

    primer hola mundo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+function Helloworld(){
+  return(
+    <p>hola mundo</p>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">este es mi primer componente 
+      <Helloworld/>
     </div>
   );
 }
